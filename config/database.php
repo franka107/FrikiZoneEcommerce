46c0ebd676f0dd40548785a2623dfd1397383d86
Ubuntu <ubuntu@ip-172-31-83-211.ec2.internal>
2019-06-19 20:20:17 +0000

    Configuracion virtual

--- config/database.php
+++ config/database.php
@@ -93,14 +93,8 @@ return [
 
         'mongodb' => [
             'driver' => 'mongodb',
-            'host' => env('DB_HOST', 'localhost'),
-            'port' => env('DB_PORT', '27017'),
             'database' => env('DB_DATABASE'),
-            'username' => env('DB_USERNAME'),
-            'password' => env('DB_PASSWORD'),
-            'options' => [
-                'database' => 'storedb'
-            ]
+	    'dsn' => env('DB_DSN'),
         ],
 
     ],
